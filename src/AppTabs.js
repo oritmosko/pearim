7679a9771ae157fdbe1218260613224a0a39ccea
unknown <oritmosko@gmail.com>
2023-09-17 15:01:06 +0300

    support tab switch, and back button

--- src/AppTabs.js
+++ src/AppTabs.js
@@ -8,28 +8,34 @@ import Reports from './Tabs/ReportsFromServer';
 import logo from './assets/logo.png';
 
 const AppTabs = () => {
-  const [selectedTab, setSelectedTab] = useState("About"); // Default selected tab is About
+  const getActiveTabName = () => {
+    const tabName = window.location.hash !== "" ? window.location.hash.split("#")[1].toLocaleLowerCase() : "";
+    return tabName !== "" ? tabName : "about";
+  }
+  const [selectedTab, setSelectedTab] = useState(getActiveTabName()); // Default selected tab is About
 
   let tabContent;
   switch (selectedTab) {
-    case "About":
+    case "about":
       tabContent = <About />;
       break;
-    case "Reports":
+    case "reports":
       tabContent = <Reports />;
       break;
-    case "Contact":
+    case "contact":
       tabContent = <Contact />;
       break;
     default:
       tabContent = <About />;
   }
 
-  const switchTabs = (tabName) => {
-    document.getElementsByClassName(selectedTab.toLocaleLowerCase())[0].classList.toggle("active");
-    setSelectedTab(tabName);
-    document.getElementsByClassName(tabName.toLocaleLowerCase())[0].classList.toggle("active");
+  const isActiveTab = (tabName) => {
+    return tabName.toLocaleLowerCase() === getActiveTabName();
   }
+  const switchTabs = () => {
+    setSelectedTab(getActiveTabName());
+  }
+  window.addEventListener('hashchange', switchTabs);
 
   return (
     <div>
@@ -49,20 +55,17 @@ const AppTabs = () => {
       </div>
       <nav className="tabs">
         <a href="#about"
-           className="tab about active"
-           onClick={() => switchTabs("About")}>
+           className={`tab about ${isActiveTab("about")  ? "active" : ""}`}>
           רקע
         </a>
         <span className="divider">|</span>
         <a href="#reports"
-           className="tab reports"
-           onClick={() => switchTabs("Reports")}>
+           className={`tab reports ${isActiveTab("reports")  ? "active" : ""}`}>
           צפייה בדוח"ות
         </a>
         <span className="divider">|</span>
         <a href="#contact"
-           className="tab contact"
-           onClick={() => switchTabs("Contact")}>
+           className={`tab contact ${isActiveTab("contact")  ? "active" : ""}`}>
           צרו קשר
         </a>
       </nav>
